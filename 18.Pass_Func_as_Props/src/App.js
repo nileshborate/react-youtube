48d36062bf46b24829dd2529c3a17f2f8524d29b
nileshborate <nileshhanumantborate@gmail.com>
2024-06-13 12:08:45 +0530

    18 changes

---
+++ 18.Pass_Func_as_Props/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Test from './Test'
+
+const App = () => {
+  return (
+    <>
+      <Test>
+        <p>This is paragraph</p>
+        <p>Para1</p>
+        <p>Para2</p>
+        <p>Para3</p>
+      </Test>
+    </>
+  )
+}
+
+export default App
---
+++ 18.Pass_Func_as_Props/src/Test.js
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+
+const Test = ({ children }) => {
+  return (
+    <>
+      <div>{children}</div>
+    </>
+  )
+}
+
+export default Test
